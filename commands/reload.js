bba9a17d96724c8db91b14f48c30df323ddace17
codefactor-io <support@codefactor.io>
2020-04-06 13:42:35 +0000

    [CodeFactor] Apply fixes

--- commands/reload.js
+++ commands/reload.js
@@ -5,30 +5,32 @@ module.exports = {
     if (!args.length) {
 
         return message.channel.send(`No command passed, ${message.author}!`);
-        
+
     }
 
     const commandName = args[0].toLowerCase();
     const command =
       message.client.commands.get(commandName) ||
       message.client.commands.find(
-        (cmd) => cmd.aliases && cmd.aliases.includes(commandName)
+        (cmd) => cmd.aliases && cmd.aliases.includes(commandName),
       );
 
-    if (!command)
-      return message.channel.send(
-        `No command named \`${commandName}\`, ${message.author}!`
+    if (!command) {
+return message.channel.send(
+        `No command named \`${commandName}\`, ${message.author}!`,
       );
+}
 
     delete require.cache[require.resolve(`./${command.name}.js`)];
 
     try {
       const newCommand = require(`./${command.name}.js`);
       message.client.commands.set(newCommand.name, newCommand);
-    } catch (error) {
+    }
+ catch (error) {
       console.log(error);
       message.channel.send(
-        `There was an error while reloading a command \`${command.name}\`:\n\`${error.message}\``
+        `There was an error while reloading a command \`${command.name}\`:\n\`${error.message}\``,
       );
     }
 
