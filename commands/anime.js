3182cd6d5c8cb64f66104abb71c03356d1e617c5
codefactor-io <support@codefactor.io>
2020-04-06 13:39:13 +0000

    [CodeFactor] Apply fixes

--- commands/anime.js
+++ commands/anime.js
@@ -11,7 +11,7 @@ module.exports = {
       .search("anime", args.join(" "), 1, { limit: 1 })
       .then(response => {
         console.log(response);
-        let myProcessedData = response.results.map(function(result) {
+        const myProcessedData = response.results.map(function(result) {
           return {
             mal_id: result.mal_id,
             title: result.title,
@@ -20,15 +20,15 @@ module.exports = {
             url: result.url,
             image_url: result.image_url,
             type: result.type,
-            score: result.score
+            score: result.score,
           };
         });
 
         const animeEmbed = new Discord.MessageEmbed()
-          //.setColor("#0099ff")
+          // .setColor("#0099ff")
           .setTitle(myProcessedData[0].title)
           .setURL(myProcessedData[0].url)
-          /*.setAuthor(
+          /* .setAuthor(
             "Some name",
             "https://i.imgur.com/wSTFkRM.png",
             "https://discord.js.org"
@@ -36,30 +36,30 @@ module.exports = {
           .setDescription(myProcessedData[0].synopsis)
           .setThumbnail(myProcessedData[0].image_url)
           .addFields(
-            //{ name: "Regular field title", value: "Some value here" },
-            //{ name: "\u200B", value: "\u200B" },
+            // { name: "Regular field title", value: "Some value here" },
+            // { name: "\u200B", value: "\u200B" },
             {
               name: "Episodes",
               value: myProcessedData[0].episodes,
-              inline: true
+              inline: true,
             },
             {
               name: "Type",
               value: myProcessedData[0].type,
-              inline: true
+              inline: true,
             },
             {
               name: "Score",
               value: myProcessedData[0].score,
-              inline: true
-            }
+              inline: true,
+            },
           )
-          /*.addField("Inline field title", "Some value here", true)
+          /* .addField("Inline field title", "Some value here", true)
           .setImage(")*/
           .setTimestamp()
           .setFooter(
             "Saber-chan",
-            "https://cdn.discordapp.com/avatars/629719032114970684/e619f816e4528964e907d369d28b63cc.jpg"
+            "https://cdn.discordapp.com/avatars/629719032114970684/e619f816e4528964e907d369d28b63cc.jpg",
           );
 
         message.channel.send(animeEmbed);
@@ -67,8 +67,8 @@ module.exports = {
       .catch(err => {
         console.error(err);
         message.channel.send(
-          "Sorry your request couldn't be executed \nPlease try later or contact vme"
+          "Sorry your request couldn't be executed \nPlease try later or contact vme",
         );
       });
-  }
+  },
 };
